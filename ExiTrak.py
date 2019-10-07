d4f467076a5f7379fc1eb071fed169939ca1031e
Agoston Walter <agoston.walter@duke.edu>
2019-10-07 19:18:08 -0400

    Added while loop

--- ExiTrak.py
+++ ExiTrak.py
@@ -6,11 +6,18 @@ class ExiTrak:
     def pinSetup(self):
         microSwitch_in = 'X1'
         p_in = pyb.Pin(microSwitch_in, pyb.Pin.IN)
-        
-        
 def main():
     myboard = ExiTrak()
-    myboard.switchSetup()
+    myboard.pinSetup()
+    while(True):
+        if(p_in.value() = 1):
+            pyb.LED(1).on()
+            pyb.delay(1000)
+            pyb.LED(1).off()
+        else:
+            pyb.LED(1).off()
+            
+        
     
 if __name__ == "__main__":
     main()
