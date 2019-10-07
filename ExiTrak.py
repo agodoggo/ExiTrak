24fdc7ccd79b183575020c47c8b5bde23a1a710d
Agoston Walter <agoston.walter@duke.edu>
2019-10-07 19:01:44 -0400

    Added pin reading

--- ExiTrak.py
+++ ExiTrak.py
@@ -3,6 +3,10 @@ class ExiTrak:
     def switchSetup(self):
         sw = pyb.Switch()
         sw.callback(lambda: pyb.LED(1).toggle())
+    def pinSetup(self):
+        microSwitch_in = 'X1'
+        p_in = pyb.Pin(microSwitch_in, pyb.Pin.IN)
+        
         
 def main():
     myboard = ExiTrak()
