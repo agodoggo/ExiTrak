e236dde64a5c44be7f68327258f933f0c4230bb3
Agoston Walter <agoston.walter@duke.edu>
2019-11-20 23:49:09 -0500

    Implemented additional functions including power management, file storage of stretches, and stretch registering. Moving towards final product.

--- ExiTrak.py
+++ ExiTrak.py
@@ -1,3 +1,4 @@
+#main.py -- put your code here!
 import pyb
 import utime
 import machine
@@ -6,12 +7,14 @@ class ExiTrak:
         self.led = pyb.LED(1)
         self.sw = pyb.Switch()
         self.p_in = None
+        self.PIN_ON = 1
     def switchSetup(self):
         sw = pyb.Switch()
         sw.callback(lambda: pyb.LED(1).toggle())
     def pinSetup(self):
         microSwitch_in = 'X1'
-        self.p_in = pyb.Pin(microSwitch_in, pyb.Pin.IN, pyb.Pin.PULL_UP)
+        self.p_in = pyb.Pin(microSwitch_in, pyb.Pin.IN, pyb.Pin.PULL_DOWN)
+        self.p_in.irq(trigger=machine.Pin.IRQ_HIGH_LEVEL, handler=Functions.stretchRegister())
     def toggleLED(self):
         led = pyb.LED(1)
         led.on()
@@ -19,28 +22,44 @@ class ExiTrak:
         led.off()
     def check_microSwitch(self):
         return (p_in.value() == 1)
+    def Sleep:
+        pyb.stop()
+        
 class Functions:
     def __init__(self):
         self.rtc = machine.RTC()
-        self.file = open("data.txt","a+")
+        self.stretchCounter = 0
+        self.hundredminutes = 60000000
+        self.tenminutes = 6000000
+        self.myPrescaler = 8399
+        self.TimerNo = 2
     def getTime(self):
-        rtc = machine.RTC()
-        # rtc.init([(2019, 10, 8, 2, 14, 46, 36, 104])
         return rtc.datetime()
-    
+    def stretchRegister(self):
+        if day has changed:
+            writeTime the last day recorded with the counter = counter
+            writeTime all the other days with the counter = 0
+        else:
+            stretchCounter++
+        myboard.toggleLED()
+        init_Timer()
+    def writeTime(self):
+        file = open("data.txt","a+")
+        file.write(stretchCounter + "\n")
+        file.close()
+        stretchCounter = 0
+    def init_Timer(self):
+        sec = pyb.Timer(TimerNo, prescaler = myPrescaler, period = hundredminutes)
+        sec.counter(0)
 def main():
     myboard = ExiTrak()
     myFunctions = Functions()
     myboard.pinSetup()
+    myFunction.initTimer()
     while(True):
-        print(myboard.p_in.value())
-        if(myboard.p_in.value() == 1):
-            print("past if statement")
-            time = myFunctions.getTime()
-            print(str(time))
-            myFunctions.file.write(str(time))
-            myboard.toggleLED()
-        # else:
-            
+        if(myFunctions.sec.counter() <= myFunctions.tenminutes):
+            myboard.Sleep()   
+        else if(myboard.check_microSwitch):
+            myFunctions.stretchRegister()           
 if __name__ == "__main__":
     main()
